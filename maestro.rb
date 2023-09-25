bf01deb585167f9904062dfcf8aabe7788e7c13c
zhaque44 <haque.zubair@gmail.com>
2023-09-25 17:56:44 -0500

    homebrew formulae for maestro
    Signed-off-by: zhaque44 <haque.zubair@gmail.com>

---
+++ maestro.rb
@@ -0,0 +1,22 @@
+class Maestro < Formula
+    desc "Maestro CLI Tool"
+    homepage "https://www.day1ops.ai"
+    baseurl = "https://github.com/argoproj/argo-rollouts/releases/download"
+    version "v1.0.0"
+
+    if OS.mac?
+      kernel = "darwin"
+      sha256 "915bc4951741e7ac0760448a48590f455ff5cc4a2ae0fd5834ded74080bc4402"
+    elsif OS.linux?
+      kernel = "linux"
+      sha256 "690cff6930282d5898ee3b13e2f446b1cf7d3a1eafbb840be9185c599ab22d73"
+    end
+
+    @@bin_name = "maestro-" + kernel + "-amd64"
+    url baseurl + "/v1.0.0/" + @@bin_name
+
+    def install
+      bin.install @@bin_name
+      mv bin/ + @@bin_name.to_s, bin/"maestro"
+    end
+end
