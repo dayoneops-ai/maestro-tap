27f07d7ce3abb2d2549a763971b3fe1756b2ead0
zhaque44 <haque.zubair@gmail.com>
2023-09-25 19:53:22 -0500

    i

--- maestro.rb
+++ maestro.rb
@@ -1,22 +1,14 @@
 class Maestro < Formula
-    desc "Maestro CLI Tool"
-    homepage "https://www.day1ops.ai"
-    baseurl = "https://github.com/argoproj/argo-rollouts/releases/download"
-    version "v1.0.0"
+  desc "Description of Maestro"
+  homepage "https://github.com/dayoneops-ai/maestro"
+  url "https://github.com/dayoneops-ai/maestro/archive/v1.0.0.tar.gz"
+  sha256 "checksum" # Calculate the actual SHA-256 checksum
 
-    if OS.mac?
-      kernel = "darwin"
-      sha256 "915bc4951741e7ac0760448a48590f455ff5cc4a2ae0fd5834ded74080bc4402"
-    elsif OS.linux?
-      kernel = "linux"
-      sha256 "690cff6930282d5898ee3b13e2f446b1cf7d3a1eafbb840be9185c599ab22d73"
-    end
+  def install
+    # Add build instructions here
 
-    @@bin_name = "maestro-" + kernel + "-amd64"
-    url baseurl + "/v1.0.0/" + @@bin_name
-
-    def install
-      bin.install @@bin_name
-      mv bin/ + @@bin_name.to_s, bin/"maestro"
-    end
+    # Install the binary: this is a go binary
+    bin.install "maestro"
+    mv bin/"maestro", bin/"maestro-1.0.0"
+  end
 end
