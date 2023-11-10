4a4217d051fbe10302ef170e94cafbda10211c23
Zubair Haque <haque.zubair@gmail.com>
2023-11-10 15:00:51 -0600

    Update maestro.rb

--- maestro.rb
+++ maestro.rb
@@ -1,21 +1,11 @@
-Class Maestro < Formula
-  desc "A command line tool for managing your deployments"
-  homepage "https://github.com/dayoneops-ai/maestro"
-  baseurl "https://github.com/dayoneops-ai/maestro/releases/tag/v1.0.0"
-  version "1.0.0"
-  sha256 "d2bf8bfd04824fde24ad40c34ffbfb972b7e2af4221d52439206cc49e38df532"
-
-  depends_on "go" => :build
-
-  @@bin_name = "maestro-" + kernel + "-amd64"
-  url baseurl + "/v1.0.0/" + @@bin_name
+class Maestro < Formula
+  desc "A CLI tool written in Viper"
+  homepage "https://github.com/your-username/maestro"
+  url "https://github.com/dayoneops-ai/maestro/releases/download/v{{ version }}/my-cli-tool-{{ version }}-darwin-amd64.tar.gz"
+  sha256 "{{ checksum }}"
+  license "MIT"
 
   def install
-    bin.install @@bin_name
-    mv bin/@@bin_name.to_s, bin/"maestro"
-  end
-
-  test do
-    system "false"
+    bin.install "maestro"
   end
 end
