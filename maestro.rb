f9c0ac228a75eab4be2e65e4dd91b5b0761b1c45
Zubair Haque <haque.zubair@gmail.com>
2023-12-22 12:52:41 -0600

    Update maestro.rb

--- maestro.rb
+++ maestro.rb
@@ -1,6 +1,6 @@
 class Maestro < Formula
   desc "A CLI tool written in Viper"
-  homepage "https://github.com/your-username/maestro"
+  homepage "https://github.com/dayoneops-ai/maestro"
   url "https://github.com/dayoneops-ai/maestro/releases/download/v{{ version }}/my-cli-tool-{{ version }}-darwin-amd64.tar.gz"
   sha256 "{{ checksum }}"
   license "MIT"
